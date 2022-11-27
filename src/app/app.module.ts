6213176a88a6612ad1360f747b2a7fc99b49b6a1
Karol Modzelewski <kar.modzelewski@gmail.com>
2022-11-27 18:10:53 +0100

    Add prettier and format the entire application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
